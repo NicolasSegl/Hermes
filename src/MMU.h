b33e0d639cad59f3977cffbc1553159474294385
Nicolas Seglenieks <nseglenieks@gmail.com>
2022-12-22 11:15:41 -0500

    Remove old unnecessary code

--- src/MMU.h
+++ src/MMU.h
@@ -15,31 +15,7 @@ private:
     uint64_t* mTicks;
 
 public:
-    union 
-    {
-        // 0x800000 = 8MB, which is the maximum size of a gameboy cartridge
-        Byte* romMemory;
-
-        // the bios takes up 0x100 bytes of instructions. it fills the memory right at the start of the cartridge
-        Byte bios[0x100];
-
-        // this structure contains the various subsets of memory found in the gameboy's cartridge
-        // more info can be found here: http://gameboy.mongenel.com/dmg/asmmemmap.html
-        struct 
-        {
-            Byte romBank0[0x4000];
-            Byte romBank1[0x4000];
-            Byte virtualRam[0x2000];
-            Byte externalRam[0x2000];
-            Byte workingRam[0x2000];
-            Byte workingRamShadow[0x1DFF];
-            Byte oam[0xA0];
-            Byte empty[0x60];
-            Byte io[0x40];
-            Byte ppu[0x40];
-            Byte zeroPageRam[0x80];
-        };
-    };
+    Byte* romMemory;
 
     // this includes vram, hram, i/o registers, etc. just any memory that is not related to the ROM memory 
     Byte ramMemory[0x8000];
